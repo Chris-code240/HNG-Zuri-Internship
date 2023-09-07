0a1b91f50c27c61939c684a9f04cb4cfb0ec0c5d
Chris-code240 <duah.marfochristian@gmail.com>
2023-09-07 01:08:26 +0100

    Day One Project1 Code base setup

--- Day One/Project1/app/views.py
+++ Day One/Project1/app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+import json
+
+@api_view(['GET'])
+def home(request):
+    return Response(json.dumps({ "slack_name": "example_name",
+            "current_day": "Monday",
+            "utc_time": "2023-08-21T15:04:05Z",
+            "track": "backend",
+            "github_file_url": "https://github.com/username/repo/blob/main/file_name.ext",
+            "github_repo_url": "https://github.com/username/repo",
+            "status_code": "200"
+        }))
 
 # Create your views here.
